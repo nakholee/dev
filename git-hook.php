6a63fdf8fc19a107d003e5eec7218ffc7bd51dfb
nakholee <nakholee@gmail.com>
2014-08-01 03:11:55 +0000

    test

--- git-hook.php
+++ git-hook.php
@@ -1,15 +1,4 @@
-<?php #!/usr/bin/env /usr/bin/php
-error_reporting(E_ALL);
-ini_set('display_errors', '1');
-set_time_limit(0);
-
-try {
-    $output = shell_exec("/var/www/html/test/git-puller.sh");
-    echo "<pre>"+$output+"</pre>";
-}
-catch(Exception $e) {
-    //log the error
-    file_put_contents('/var/www/html/test/logs/github.txt', $e . ' ' . $output, FILE_APPEND);
-    exit(0);
-}
+<?php
+    $output = shell_exec("ls -la");
+    echo "<pre>$output</pre>";
 ?>
